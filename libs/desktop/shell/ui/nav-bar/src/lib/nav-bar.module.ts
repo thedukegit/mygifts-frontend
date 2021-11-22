6511a966cb69f2c87217e3e2918f9d401cffcda1
emielh <e.hertogs@anva.nl>
2021-11-22 23:05:41 +0100

    generated the nav-bar component

--- libs/desktop/shell/ui/nav-bar/src/lib/nav-bar.module.ts
+++ libs/desktop/shell/ui/nav-bar/src/lib/nav-bar.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavBarComponent } from './nav-bar.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [NavBarComponent],
 })
 export class NavBarModule {}
