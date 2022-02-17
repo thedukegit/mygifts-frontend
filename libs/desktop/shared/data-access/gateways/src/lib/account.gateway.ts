9aa5c0b3218a385f6f3ac1f8399159b478bb8a8f
emielh <e.hertogs@anva.nl>
2022-02-17 22:18:55 +0100

    added interface to replace the any keywork as return type also added private function for readability

--- libs/desktop/shared/data-access/gateways/src/lib/account.gateway.ts
+++ libs/desktop/shared/data-access/gateways/src/lib/account.gateway.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { catchError } from 'rxjs/operators';
+import { Account } from '../../../domains/src/lib/account.interface';
 
 @Injectable({ providedIn: 'root' })
 export class AccountGateway {
@@ -10,9 +11,12 @@ export class AccountGateway {
 
   constructor(private http: HttpClient) {}
 
-  public signInWithPassword(email: string, password: string): Observable<any> {
+  public signInWithPassword(
+    email: string,
+    password: string
+  ): Observable<Account> {
     return this.http
-      .post(
+      .post<Account>(
         `${this.accountResource}:signInWithPassword?key=AIzaSyCS0pevxJiFspRAlGsEc2DrLi89vDDsDhw`,
         {
           email: email,
@@ -23,25 +27,31 @@ export class AccountGateway {
       .pipe(
         catchError((err) => {
           let errorMessage = 'Un unknown error occurred';
-          if (!err.error?.error?.message) {
-            return throwError(errorMessage);
+          if (!err.error.error.message) {
+            return throwError(new Error(errorMessage));
           }
-          switch (err.error.error.message) {
-            case 'INVALID_EMAIL':
-              errorMessage = 'The email address was invalid';
-              break;
-            case 'EMAIL_NOT_FOUND':
-              errorMessage = 'The email address was not found';
-              break;
-            case 'INVALID_PASSWORD':
-              errorMessage = 'Invalid password';
-              break;
-            case 'USER_DISABLED':
-              errorMessage = 'This account has been disabled';
-              break;
-          }
-          return throwError(errorMessage);
+          errorMessage = this.createSensibleMessage(err.error.error.message);
+          return throwError(new Error(errorMessage));
         })
       );
+  }
+
+  private createSensibleMessage(message: string): string {
+    let sensibleMessage: string = '';
+    switch (message) {
+      case 'INVALID_EMAIL':
+        sensibleMessage = 'The email address was invalid';
+        break;
+      case 'EMAIL_NOT_FOUND':
+        sensibleMessage = 'The email address was not found';
+        break;
+      case 'INVALID_PASSWORD':
+        sensibleMessage = 'Invalid password';
+        break;
+      case 'USER_DISABLED':
+        sensibleMessage = 'This account has been disabled';
+        break;
+    }
+    return sensibleMessage;
   }
 }
