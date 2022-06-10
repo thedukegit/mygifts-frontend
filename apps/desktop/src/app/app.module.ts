0f10b11403a2916e03abc5922184583acb7a8410
emielh <e.hertogs@anva.nl>
2022-06-10 16:18:45 +0200

    put routes in a constant and added authguard to certain routes

--- apps/desktop/src/app/app.module.ts
+++ apps/desktop/src/app/app.module.ts
@@ -2,22 +2,54 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
-import { AuthComponent } from '@mygifts/desktop-auth-feature-auth';
-import { LayoutComponent } from '@mygifts/desktop/shell/ui/layout';
+import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent, AuthGuard } from '@mygifts/desktop-auth-feature-auth';
 import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from '@mygifts/desktop/home';
+import { GiftsComponent } from '@mygifts/desktop/gifts/feature-gifts';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    redirectTo: '/gifts',
+    pathMatch: 'full',
+  },
+  {
+    path: 'gifts',
+    component: GiftsComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'home',
+    children: [
+      {
+        path: '',
+        component: HomeComponent,
+        pathMatch: 'full',
+        canActivate: [AuthGuard],
+      },
+    ],
+  },
+  {
+    path: 'auth',
+    children: [
+      {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full',
+      },
+      {
+        path: 'login',
+        redirectTo: 'AuthComponent',
+      },
+    ],
+    component: AuthComponent,
+  },
+];
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    RouterModule.forRoot([
-      { path: '', component: LayoutComponent, pathMatch: 'full' },
-      { path: 'auth', component: AuthComponent, pathMatch: 'full' },
-    ]),
-  ],
-  providers: [],
+  imports: [BrowserModule, HttpClientModule, RouterModule.forRoot(appRoutes)],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
