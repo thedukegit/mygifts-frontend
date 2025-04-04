78e54e4883a3145a430f11e7b4098511577cdad4
emielh <e.hertogs@anva.nl>
2025-04-04 17:17:20 +0200

    Revert "added primeng apollo theme"
    This reverts commit 09e2d6853fae90a6186e6cbc08abdaa58ee8ca1c.

--- apps/mygifts/src/app/app.config.ts
+++ apps/mygifts/src/app/app.config.ts
@@ -1,19 +1,10 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { providePrimeNG } from 'primeng/config';
-import Aura  from '@primeng/themes/aura';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
-    provideAnimationsAsync(),
-    providePrimeNG({
-      theme: {
-        preset: Aura
-      }
-    })
   ],
 };
--- apps/mygifts/src/app/app.routes.ts
+++ apps/mygifts/src/app/app.routes.ts
@@ -1,9 +1,3 @@
 import { Route } from '@angular/router';
-import { AppLayout } from './layout/components/app.layout';
 
-export const appRoutes: Route[] = [
-  {
-    path: '',
-    component: AppLayout,
-  }
-];
+export const appRoutes: Route[] = [];
