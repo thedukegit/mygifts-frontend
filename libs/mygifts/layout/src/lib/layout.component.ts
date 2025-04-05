fb6f4dde1489869d54917d8126384bee9e28e02b
emielh <e.hertogs@anva.nl>
2025-04-05 12:30:14 +0200

    started to re-implement apollo theme, in baby steps

--- libs/mygifts/layout/src/lib/layout.component.ts
+++ libs/mygifts/layout/src/lib/layout.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
 
 /** responsible for the common components of the screen, like top bar, menu, etc. **/
 @Component({
   selector: 'mg-layout',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css',
 })
