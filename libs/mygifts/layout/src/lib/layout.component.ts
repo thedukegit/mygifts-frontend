882822e1c4889b4f6267ff27c5b64f3476b52077
emielh <e.hertogs@anva.nl>
2025-04-05 08:25:18 +0200

    comment

--- libs/mygifts/layout/src/lib/layout.component.ts
+++ libs/mygifts/layout/src/lib/layout.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+/** responsible for the common components of the screen, like top bar, menu, etc. **/
 @Component({
   selector: 'lib-layout',
   imports: [CommonModule],
