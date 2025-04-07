44a5530f944bc247b73a3cdb9548986bbb1430c8
emielh <e.hertogs@anva.nl>
2025-04-07 18:11:41 +0200

    added a sidenav

--- apps/mg-frontend/src/app/app.component.ts
+++ apps/mg-frontend/src/app/app.component.ts
@@ -1,8 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  imports: [RouterModule],
+  imports: [
+    RouterModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    MatButtonModule,
+    MatDividerModule,
+    CommonModule,
+  ],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
