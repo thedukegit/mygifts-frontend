4be20e9030f503e2d1ceb0583251e8c7a86325ec
emielh <e.hertogs@anva.nl>
2025-04-08 21:00:47 +0200

    added a list component to show a list of gifts

--- libs/mg-frontend/list/eslint.config.mjs
+++ libs/mg-frontend/list/eslint.config.mjs
@@ -12,7 +12,7 @@ export default [
         'error',
         {
           type: 'attribute',
-          prefix: 'lib',
+          prefix: 'mg',
           style: 'camelCase',
         },
       ],
@@ -20,7 +20,7 @@ export default [
         'error',
         {
           type: 'element',
-          prefix: 'lib',
+          prefix: 'mg',
           style: 'kebab-case',
         },
       ],
