1922cbaa8a0ffb07faeb37c223842debab98b381
emielh <e.hertogs@anva.nl>
2025-07-08 13:59:52 +0200

    Implement sorting functionality for gifts

--- libs/mg-frontend/list/src/lib/list.component.ts
+++ libs/mg-frontend/list/src/lib/list.component.ts
@@ -1,15 +1,15 @@
-import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatCardModule } from '@angular/material/card';
+import { Component, inject, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatCardModule } from '@angular/material/card';
 import { MatDialog } from '@angular/material/dialog';
-import { Gift } from './gift.interface';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { AddGiftDialogComponent } from './add-gift-dialog/add-gift-dialog.component';
-import { GIFT_REPOSITORY } from './repositories/gift-repository.token';
+import { Gift } from './gift.interface';
 import { GiftRepository } from './repositories/gift-repository.interface';
+import { GIFT_REPOSITORY } from './repositories/gift-repository.token';
 import { IndexedDbGiftRepository } from './repositories/indexed-db-gift-repository';
 
 @Component({
@@ -31,11 +31,35 @@ export class ListComponent implements OnInit {
   viewMode: 'list' | 'grid' = 'grid';
   protected gifts: Gift[] = [];
 
+  sortBy: 'name' | 'price' = 'name';
+  sortDirection: 'asc' | 'desc' = 'asc';
+
   private readonly giftRepository = inject<GiftRepository>(GIFT_REPOSITORY);
   private readonly dialog = inject(MatDialog);
 
   async ngOnInit() {
     this.gifts = await this.giftRepository.getAll();
+  }
+
+  get sortedGifts(): Gift[] {
+    return [...this.gifts].sort((a, b) => {
+      let compare = 0;
+      if (this.sortBy === 'name') {
+        compare = a.name.localeCompare(b.name);
+      } else if (this.sortBy === 'price') {
+        compare = a.price - b.price;
+      }
+      return this.sortDirection === 'asc' ? compare : -compare;
+    });
+  }
+
+  setSort(by: 'name' | 'price') {
+    if (this.sortBy === by) {
+      this.sortDirection = this.sortDirection === 'asc' ? 'desc' : 'asc';
+    } else {
+      this.sortBy = by;
+      this.sortDirection = 'asc';
+    }
   }
 
   toggleViewMode() {
